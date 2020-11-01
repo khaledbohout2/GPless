1548719c2c671c1d8b45cc00164c62c380c8043c
Khaled Bohout <khaledbohout2012@>
2020-11-01 11:52:21 +0200

    first commit

---
+++ GPless/Controller/WelcomeVC.swift
@@ -0,0 +1,29 @@
+//
+//  WelcomeVC.swift
+//  GPless
+//
+//  Created by Khaled Bohout on 11/1/20.
+//
+
+import UIKit
+
+class WelcomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
